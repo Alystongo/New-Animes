5d5d4fcfc78722c8d638cdfd024c344caae170a2
Alystongo <98561727+Alystongo@users.noreply.github.com>
2022-05-18 22:30:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,382 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+$(document).ready(function(){
+	
+	$("#Home").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").show();
+		$("img").show();
+		$(".animes").hide();
+		
+	});
+	
+	$("#Eighty").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+		
+		$(".animes").hide();
+		$("#eighty").show();
+				
+		
+	});
+	
+	$("#Ano").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#ano").show();		
+	
+	});
+	
+	
+	$("#Araburu").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#araburu").show();		
+	
+	});
+	
+	$("#Baki").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#baki").show();		
+	
+	});
+	
+	$("#Black").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#black").show();		
+	
+	});
+	
+	$("#Bleach").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#bleach").show();		
+	
+	});
+	
+	$("#Hero").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#hero").show();		
+	
+	});
+	
+	$("#Chuu").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#chuni").show();		
+	
+	});
+	
+	$("#Dragon").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#dragon").show();		
+	
+	});
+	
+	$("#Dororo").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#dororo").show();		
+	
+	});
+	
+	$("#Erased").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#erased").show();		
+	
+	});
+	
+	$("#Evan").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#evan").show();		
+	
+	});
+	
+	$("#Fire").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#fire").show();		
+	
+	});
+	
+	$("#Fullmetal").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#fullmetal").show();		
+	
+	});
+	
+	$("#Haikyuu").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#haiky").show();		
+	
+	});
+	
+	$("#Idaten").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#idaten").show();		
+	
+	});
+	
+	$("#Karakuri").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#circus").show();		
+	
+	});
+	
+	$("#Hitman").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#katakyo").show();		
+	
+	});
+	
+	$("#Kimetsu").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#kimetsu").show();		
+	
+	});
+	
+	$("#Kono").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#kono").show();		
+	
+	});
+	
+	$("#Kuroko").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#kuroko").show();		
+	
+	});
+	
+	$("#Mob").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#mob").show();		
+	
+	});
+	
+	$("#Mugen").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#mugen").show();		
+	
+	});
+	
+	$("#Naruto").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#naruto").show();		
+	
+	});
+	
+	$("#Piece").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#piece").show();		
+	
+	});
+	
+	$("#Punch").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#punch").show();		
+	
+	});
+	
+	$("#Shingeki").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#shingeki").show();		
+	
+	});
+	
+	$("#Violet").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#violet").show();		
+	
+	});
+	
+	$("#Saga").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#viland").show();		
+	
+	});
+	
+	$("#Hakusho").click(function(){
+		$('.videos').trigger('pause');
+		
+		$(".wrapper").hide();
+		$("img").hide();
+		$(".icon").click();
+	
+		$(".animes").hide();
+		$("#hakusho").show();		
+	
+	});
+});
